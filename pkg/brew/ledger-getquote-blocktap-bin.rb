91c1fd4ec319d2e610e6a487ea626c00fe70bb55
Colin Dean <git@cad.cx>
2019-01-19 21:08:00 -0500

    Updates Homebrew formula to 0.0.7

--- pkg/brew/ledger-getquote-blocktap-bin.rb
+++ pkg/brew/ledger-getquote-blocktap-bin.rb
@@ -1,14 +1,14 @@
 class LedgerGetquoteBlocktapBin < Formula
-    version '0.0.5'
+    version '0.0.7'
     desc "Retrieve spot pricing for cryptocurrencies using the Blocktap.io GraphQL API"
     homepage "https://github.com/colindean/ledger-getquote-blocktap"
 
     if OS.mac?
       url "https://github.com/colindean/ledger-getquote-blocktap/releases/download/#{version}/ledger-getquote-blocktap-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "afb0f579fa7a447589748b7548b9a718f98940a414ff75185d71bf08ee31e4fb"
+      sha256 "d8a8b7dd442aa5d12b1838ae355cbb52bc0ecef2d1dd7f31d10982933a67f003"
     elsif OS.linux?
       url "https://github.com/colindean/ledger-getquote-blocktap/releases/download/#{version}/ledger-getquote-blocktap-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2addbe1cf6edc8c9dbd8e9b2329f396683308f528a9a63df0514db6fe9bd9d54"
+      sha256 "aff8c32e257ceadcede201ea9e71e65bb905094dabeb0bd6bbb1f6b3277e775b"
     end
 
     def install
