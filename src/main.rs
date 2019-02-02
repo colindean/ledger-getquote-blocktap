2f57125654a2e8066979a95f849c8050368d13be
Colin Dean <git@cad.cx>
2019-02-01 22:27:44 -0500

    Renames Currency to Asset to reflect API changes
    See https://blog.altangent.com/blocktap-update-1/ for details

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use argparse::{ArgumentParser, Store};
     query_path = "schema/single.graphql",
     response_derives = "Debug"
 )]
-pub struct SingleCurrency;
+pub struct SingleAsset;
 
 const BLOCKTAP_GRAPHQL_ENDPOINT: &str = "https://api.blocktap.io/graphql";
 
@@ -25,30 +25,30 @@ fn main() {
     let mut symbol = String::from("");
     {
         let mut parser = ArgumentParser::new();
-        parser.set_description("Retrieves last price for a currency from Blocktap.io");
+        parser.set_description("Retrieves last price for a asset from Blocktap.io");
         parser
             .refer(&mut symbol)
-            .add_argument("symbol", Store, "Symbol of the currency");
+            .add_argument("symbol", Store, "Symbol of the asset");
         parser.parse_args_or_exit();
     }
     eprintln!("Accessing {}…", BLOCKTAP_GRAPHQL_ENDPOINT);
     let response_body =
-        do_query(single_currency::Variables { symbol: symbol }).expect("Unable to query");
+        do_query(single_asset::Variables { symbol: symbol }).expect("Unable to query");
     let price: String = extract_price(response_body);
     println!("{}", price)
 }
 
-use single_currency::SingleCurrencyMarkets;
-fn extract_price(response: Response<single_currency::ResponseData>) -> String {
-    let markets: Vec<Option<SingleCurrencyMarkets>> = response
+use single_asset::SingleAssetMarkets;
+fn extract_price(response: Response<single_asset::ResponseData>) -> String {
+    let markets: Vec<Option<SingleAssetMarkets>> = response
         .data
         .expect("No data")
-        .currency
-        .expect("No currency")
+        .asset
+        .expect("No asset")
         .markets
         .expect("No markets");
-    let first: &Option<SingleCurrencyMarkets> = markets.first().expect("Markets is empty");
-    let markets: &SingleCurrencyMarkets = first.as_ref().expect("sdf");
+    let first: &Option<SingleAssetMarkets> = markets.first().expect("Markets is empty");
+    let markets: &SingleAssetMarkets = first.as_ref().expect("sdf");
 
     let ticker = markets.ticker.as_ref().expect("No ticker");
     let price = ticker.last_price.as_ref().expect("No last_price");
@@ -57,14 +57,14 @@ fn extract_price(response: Response<single_currency::ResponseData>) -> String {
 }
 
 fn do_query(
-    variables: single_currency::Variables,
-) -> Result<Response<single_currency::ResponseData>, failure::Error> {
-    let request_body = SingleCurrency::build_query(variables);
+    variables: single_asset::Variables,
+) -> Result<Response<single_asset::ResponseData>, failure::Error> {
+    let request_body = SingleAsset::build_query(variables);
     let client = reqwest::Client::new();
     let mut resp = client
         .post(BLOCKTAP_GRAPHQL_ENDPOINT)
         .json(&request_body)
         .send()?;
-    let response_body: Response<single_currency::ResponseData> = resp.json()?;
+    let response_body: Response<single_asset::ResponseData> = resp.json()?;
     Ok(response_body)
 }
